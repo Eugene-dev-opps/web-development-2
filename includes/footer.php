721b0b0d10711ee8443d7a71019c8ae379bc6519
Eugene Moti <78027361+Eugene-dev-opps@users.noreply.github.com>
2021-11-17 20:36:24 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<div class="footer-last">
+	<div class="container" style="text-align:center;">
+        Copyright &copy; <?php echo date("Y"); ?>. <?php echo $siteTitle ?>. All rights reserved.
+    </div><!-- container -->
+</div><!-- footer-last -->
---
+++ includes/header.php
@@ -0,0 +1,40 @@
+<div class="navbar-wrapper" id="header">
+	<div class="container">
+    <nav class="navbar navbar-inverse navbar-static-top" id="navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+        	<a href="#">
+            	<img src="<?php echo $siteUrl; ?>img/all.jpg" width="250" style="margin-top:10px;" />
+            </a>
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+          	<li><a href="<?php echo $siteUrl; ?>index.php">Home</a></li>
+          	<?php
+          	if(isset($_SESSION['username']))
+			{
+			?>
+			<li><a href="<?php echo $siteUrl; ?> members/">Dashboard</a></li>
+			<li><a href="<?php echo $siteUrl; ?> members/profile.php">My Profile</a></li>
+			<li><a href="<?php echo $siteUrl; ?> login.php"><i class="ion-log-out"></i> Logout</a></li>
+			<?php
+			}
+			else {
+			?>
+			<li><a href="<?php echo $siteUrl; ?>login.php">Login</a></li>
+			<li><a href="<?php echo $siteUrl; ?>register.php">Register</a></li>
+			<li><a href="<?php echo $siteUrl; ?>how-it-works.php">How It Works</a></li>
+			<?php
+			}
+			?>
+            <li><a href="<?php echo $siteUrl; ?>about-us.php">About Us</a></li>
+            <li><a href="<?php echo $siteUrl; ?>faqs.php">FAQs</a></li>
+			<li><a href="<?php echo $siteUrl; ?>contact.php">Support</a></li>
+			<li><a href="<?php echo $siteUrl; ?>vendor.php">Vendor</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    </div><!-- container -->
+</div><!-- header -->
